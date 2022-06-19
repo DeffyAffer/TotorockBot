346c6c05b55174735225060bb2d61699db42aa92
DeffyAffer <hilairebenamorsyan@gmail.com>
2022-06-19 17:48:53 +0200

    Test réseaux

--- commands/socials/instagram.js
+++ commands/socials/instagram.js
@@ -3,15 +3,37 @@ const { MessageEmbed } = require('discord.js');
 module.exports = {
   name: "instagram",
   category: 'socials',
-  permissions: ['SEND_MESSAGES'],
+  permissions: ['SEND_MESSAGES'], // ²
   ownerOnly: false,
   usage: 'instagram',
   examples: ['instagram'],
   description: "Renvoie les liens des comptes Instagram de Totorock!",
   async run(client, message, args) {  
-    await message.reply('📷¹ : https://www.instagram.com/totorock1/\n📷² : https://www.instagram.com/totorockphotos/')
+    const embed1 = new MessageEmbed()
+      .setTitle('📷¹')
+      .setURL('https://www.instagram.com/totorock1/')
+      .setTimestamp()
+      .setFooter({ text: message.author.username, iconURL: message.author.displayAvatarURL() });
+
+      const embed2 = new MessageEmbed()
+      .setTitle('📷²')
+      .setURL('https://www.instagram.com/totorockphotos/')
+      .setTimestamp()
+      .setFooter({ text: message.author.username, iconURL: message.author.displayAvatarURL() });
   },
   async runInteraction(client, interaction) {
-    await interaction.reply('📷¹ : https://www.instagram.com/totorock1/\n📷² : https://www.instagram.com/totorockphotos/')
+    const embed1 = new MessageEmbed()
+      .setTitle('📷¹')
+      .setURL('https://www.instagram.com/totorock1/')
+      .setTimestamp()
+      .setFooter({ text: message.author.username, iconURL: message.author.displayAvatarURL() });
+
+      const embed2 = new MessageEmbed()
+      .setTitle('📷²')
+      .setURL('https://www.instagram.com/totorockphotos/')
+      .setTimestamp()
+      .setFooter({ text: message.author.username, iconURL: message.author.displayAvatarURL() });
+    
+      interaction.reply({ content: ' ', embeds: [embed1, embed2] })
   }
 };
