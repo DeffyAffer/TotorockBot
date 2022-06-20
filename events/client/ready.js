f3cb4678d1455edfa90db5fc8e62f5dadc1695b8
DeffyAffer <hilairebenamorsyan@gmail.com>
2022-06-20 17:41:06 +0200

    Multi-status

--- events/client/ready.js
+++ events/client/ready.js
@@ -23,7 +23,7 @@ module.exports = {
     client.user.setPresence({ activities: [{ name: 'https://twitch.tv/totorock1', type: 'STREAMING' }] })
     client.user.setPresence({ activities: [{ name: 'les clips les plus drôles de Totorock', type: 'WATCHING' }] })
     client.user.setPresence({ activities: [{ name: 'Totorock rager', type: 'LISTENING' }] })
-    client.user.setPresence({ activities: [{ name: 'se pose des questions sur la relativité de l\'existence de la Terre' }] })
+    client.user.setPresence({ activities: [{ name: 'se poser des questions sur la relativité de l\'existence de la Terre' }] })
     
   }, 5000)
 
