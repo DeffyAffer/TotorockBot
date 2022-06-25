15a5e06fa079b2b939a4a11be9b1bf09ad343df8
DeffyAffer <hilairebenamorsyan@gmail.com>
2022-06-25 16:43:46 +0200

    Correction commande mute

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -65,8 +65,8 @@ module.exports = {
       )
       .setTimestamp();
 
-    interaction.channel.reply(`${member} a été mute pour la raison suivante: ${reason} (${duration})`);
-    await logChannel.reply({ embeds: [embed] });
+    interaction.channel.send(`${member} a été mute pour la raison suivante: ${reason} (${duration})`);
+    await logChannel.send({ embeds: [embed] });
     await interaction.reply({
       content: `Le membre ${member} a été mute pour ${duration} car ${reason}!`,
       ephemeral: true,
