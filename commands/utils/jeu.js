3f7f31da3025d08172c38d91b6d8725e0deaa57c
DeffyAffer <hilairebenamorsyan@gmail.com>
2022-07-02 14:47:59 +0200

    .

--- commands/utils/jeu.js
+++ commands/utils/jeu.js
@@ -27,7 +27,7 @@ module.exports = {
      async runInteraction(client, interaction)  {
       
       const embed = new MessageEmbed()
-        .setTitle('Sondage jeu du dimanche')
+        .setTitle('Sondage jeu du dimanche <@&987853422499868693>')
         .setColor('#f7d1b8')
         .setDescription(`⚽ - Mario Strikers : Battle League\n🥊 - Super Smash Bros Ultimate\n🌍 - GeoGuessr\n🤸 - Fall Guys`)
         .setTimestamp()
